c085333384ea8d8bbfb5aa8b73e771f9f20025e3
jmartorell <jmartorell@captio.com>
2021-05-20 13:35:15 +0200

    login form

---
+++ src/app/shared/components/footer/footer.component.ts
@@ -0,0 +1,26 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.css']
+})
+export class FooterComponent implements OnInit {
+
+  public currentYear: string;
+  
+  constructor() { 
+    this.currentYear = '';
+  }
+
+  ngOnInit(): void {
+    this.getCurrentYear();
+  }
+
+  getCurrentYear(): void {
+    // Se actualiza en el footer el año actual
+    this.currentYear = new Date().getFullYear().toString();
+  }
+
+}
+
