97c8429d0438b2c1db95b36d9b1713b407da5605
jmartorell <jmartorell@captio.com>
2021-06-08 18:40:33 +0200

    purchases admin

---
+++ src/app/backoffice/components/payment-methods-admin/payment-methods-admin.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-payment-methods-admin',
+  templateUrl: './payment-methods-admin.component.html',
+  styleUrls: ['./payment-methods-admin.component.css']
+})
+export class PaymentMethodsAdminComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
